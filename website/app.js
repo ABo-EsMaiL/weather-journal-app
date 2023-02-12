7ec31b346842576c2532a8d5a5c07040d0ebc515
ABo-EsMaiL <123252905+ABo-EsMaiL@users.noreply.github.com>
2023-02-12 06:14:49 +0200

    Add files via upload

---
+++ website/app.js
@@ -0,0 +1,72 @@
+/* Global Variables */
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = ',&appid=d725d4d8e5129e02ef33773a1e315a84';
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// let newDate = d.toDateString();
+
+// EVENT LISTENER TO ADD FUNCTION TO BUTTON
+document.getElementById('generate').addEventListener('click',performAction)
+
+// FUNCTION CALLED BY LISTENER
+function performAction(e){
+    const zipFact =  document.getElementById('zip').value;
+    const feeling =  document.getElementById('feelings').value;
+    getWeather(baseURL,zipFact, apiKey)
+    // New Syntax!
+    .then(function(data){
+        console.log(data);
+        // Add data
+        postData('/add', {date:newDate, temp:data.main.temp , content:feeling});
+        // We can do this because of Async!
+        updateUI()
+    })
+}
+
+// FUNCTION TO GET WEB API DATA
+const getWeather = async (baseURL, zip, key)=>{
+
+    const res = await fetch(baseURL+zip+key);
+        try {
+            const data = await res.json();
+            console.log(data)
+            // 1. We can do something with our returned data here-- like chain promises!
+            return data
+        }  catch(error) {
+            // appropriately handle the error
+            console.log("error", error);
+        }
+}
+
+// FUNCTION TO POST API DATA
+const postData= async (url='',data={})=>{
+    const response=await fetch(url ,{
+        method:'POST',
+        credentials:'same-origin',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify(data),
+    });
+    try{
+        const newData=await response.json();
+        console.log(newData)
+        return newData
+    }catch(error){
+        console.log('error',error);
+    }
+}
+
+// FUNCTION TO GET PROJECT DATA
+const updateUI = async () => {
+    const request = await fetch('/all');
+    try{
+        const allData = await request.json();
+        document.getElementById('date').innerHTML = `Date: ${allData[0].date}`;
+        document.getElementById('temp').innerHTML = `Temperature: ${allData[0].temp}`;
+        document.getElementById('content').innerHTML = `Feeling: ${allData[0].content}`;
+    }catch(error){
+        console.log("error", error);
+    }
+}
